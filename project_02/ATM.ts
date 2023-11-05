3d768b7f970c060bf5078fe460b43761efa0c8d8
YahyaFaraz <120309033+YahyaFaraz@users.noreply.github.com>
2023-11-05 13:00:43 +0500

    Add files via upload

---
+++ project_02/ATM.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+async function Detail(user : string = 'YahyaFaraz', code : string = "7777" ) {
+    console.log('Welcome')
+    const bank_details  = await inquirer.prompt(
+        [{
+        type: "input",
+        name: "user_id",
+        message: "Please enter your User ID: "
+
+
+    },
+    {
+        type: "input",
+        name: "user_pin",
+        message: "Please enter your PIN: "
+
+    }]);
+    if (bank_details.user_id === user && bank_details.user_pin === code ){
+        console.log(chalk.blueBright('ATM Unlocked.'))
+
+    }else{
+        console.log(chalk.red('ATM Not Accessible. Your user_id or PIN is incorrect.'))
+    }
+
+}
+Detail()
